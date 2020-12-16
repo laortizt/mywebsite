84e291ccf748ddc9588a088ea8fa7a173fd40f08
laortizt <adri26.ortiz@gmail.com>
2020-12-16 17:39:55 -0500

    cambio de slider por iconos

--- js/designesia.js
+++ js/designesia.js
@@ -181,11 +181,8 @@
              });
          });
 
-         $('.images-popup').magnificPopup({
-             delegate: 'a', // child items selector, by clicking on it popup will open
-             type: 'image'
-             // other options
-         });
+         
+         
      }
      /* --------------------------------------------------
       * plugin | enquire.js
